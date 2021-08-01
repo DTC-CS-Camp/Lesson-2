3cdb0fb2a0ac88bc7dfc09629ea19fc8c0bacc34
DTC-CS-Camp <68837059+DTC-CS-Camp@users.noreply.github.com>
2021-08-01 11:22:16 -0600

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,10 +1,22 @@
-//Write your class for Problem 1 in here
-//Make sure you have correct syntax for each
-//your methods, especially spelling, since otherwise
-//when you run the program it will fail. Make sure to add 
-//the public modifier before the name of the class, 
-//otherwise the test cases will fail. 
-
+import java.io.*;
+import java.util.*;
+//Create your Shape class for problem 1 below!
 //EDIT CODE BELOW \/
+public class Shape{
+  private double sides;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 //EDIT CODE ABOVE /\
